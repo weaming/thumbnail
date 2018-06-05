ab43b573ff56f618515d215f1b26d9bc1dfe395b
weaming <garden.yuen@gmail.com>
2018-06-05 13:32:35 +0800

    change option name

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 )
 
 func main() {
-	var INDIR = flag.String("i", ".", "The input directory to be thumnail.")
-	var OUTDIR = flag.String("o", "", "The output directory to save thumnail. Default [$INPUT/../thumbnail]")
+	var INPUT = flag.String("i", ".", "The input directory or image to be thumnail.")
+	var OUTDIR = flag.String("o", "", "The output directory to save thumnail. Default [dir($INPUT)/thumbnail]")
 	var MAX_WIDTH = flag.Uint("width", 1280, "The maximum width of output photo.")
 	var MAX_HEIGHT = flag.Uint("height", 1280, "The maximum height of output photo.")
 	var outdir = ""
@@ -21,16 +21,16 @@ func main() {
 	if *OUTDIR != "" {
 		outdir = *OUTDIR
 	} else {
-		outdir = fp.Join(fp.Dir(*INDIR), "thumbnail")
+		outdir = fp.Join(fp.Dir(*INPUT), "thumbnail")
 	}
 
 	start := time.Now()
-	if libfs.IsDir(*INDIR) {
-		fmt.Printf("To be process direcotry: [%v]\n", *INDIR)
-		count, _ := thumb_directory(*INDIR, outdir, *MAX_WIDTH, *MAX_HEIGHT)
+	if libfs.IsDir(*INPUT) {
+		fmt.Printf("To be process direcotry: [%v]\n", *INPUT)
+		count, _ := thumb_directory(*INPUT, outdir, *MAX_WIDTH, *MAX_HEIGHT)
 		fmt.Printf("Processed photos: %v; Time cost: %v\n", count, time.Since(start))
-	} else if libfs.IsFile(*INDIR) {
-		fmt.Printf("To be process image: %v\n", *INDIR)
+	} else if libfs.IsFile(*INPUT) {
+		fmt.Printf("To be process image: %v\n", *INPUT)
 		if !libfs.Exist(outdir) {
 			err := os.MkdirAll(outdir, 0755)
 			if err != nil {
@@ -38,7 +38,7 @@ func main() {
 				os.Exit(1)
 			}
 		}
-		err := thumbnail(*INDIR, fp.Join(outdir, fp.Base(*INDIR)), *MAX_WIDTH, *MAX_HEIGHT)
+		err := thumbnail(*INPUT, fp.Join(outdir, fp.Base(*INPUT)), *MAX_WIDTH, *MAX_HEIGHT)
 		if err != nil {
 			fmt.Printf("failed thumbnail: %v\n", err)
 		} else {
